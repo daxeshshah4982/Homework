1fac549c50c7117f396c4cd7c2bb2b07fa0e94ce
Samsung <daxeshshah4982@gmail.com>
2019-10-16 23:56:52 +0100

    registration

---
+++ seleniumHomework12_10_19/src/Select.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Select {
+    WebDriver driver;
+
+    public Select(WebDriver driver) {
+        this.driver = driver;
+    }
+    public void selectWebElementFromDropDown(By by){
+        driver.findElement(by).click();
+    }
+    public void selectcheckbox(By by){
+        driver.findElement(by).click();
+    }
+
+}
